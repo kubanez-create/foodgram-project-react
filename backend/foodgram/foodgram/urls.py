1ed616b13f9da531f2f848e7742115f26599d838
kubanez-create <zo22@mail.ru>
2023-04-07 22:37:56 +0400

    commented out media from url

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('api/', include('api.urls', namespace="api")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
